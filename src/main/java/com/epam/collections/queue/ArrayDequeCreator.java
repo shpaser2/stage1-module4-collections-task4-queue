20878b9d91857061bc249eb4f96a837c33643aba
Siarhei_Shpakouski <siarhei_shpakouski@epam.com>
2023-05-15 20:56:50 +0400

    add working solutions

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,37 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+        arrayDeque.addLast(Objects.requireNonNull(firstQueue.poll()));
+        arrayDeque.addLast(Objects.requireNonNull(firstQueue.poll()));
+        arrayDeque.addLast(Objects.requireNonNull(secondQueue.poll()));
+        arrayDeque.addLast(Objects.requireNonNull(secondQueue.poll()));
+
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            firstQueue.add(arrayDeque.pollLast());
+            if (!firstQueue.isEmpty()) {
+                arrayDeque.addLast(firstQueue.poll());
+            }
+            if (!firstQueue.isEmpty()) {
+                arrayDeque.addLast(firstQueue.poll());
+            }
+
+            secondQueue.add(arrayDeque.pollLast());
+            if (!secondQueue.isEmpty()) {
+                arrayDeque.addLast(secondQueue.poll());
+            }
+            if (!secondQueue.isEmpty()) {
+                arrayDeque.addLast(secondQueue.poll());
+            }
+        }
+
+        return arrayDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> orderOfEating = new ArrayList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < everyDishNumberToEat - 1; i++) {
+                queue.add(queue.poll());
+            }
+            orderOfEating.add(queue.poll());
+        }
+
+        return orderOfEating;
     }
 }
